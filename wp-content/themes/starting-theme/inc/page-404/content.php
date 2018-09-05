e7ca5f9dd09530c677e700c1db42d447ab535f4f
Web Dev <webdev@Webs-MBP.lan>
2018-09-05 09:27:32 +0100

    404 redesigned

---
+++ wp-content/themes/starting-theme/inc/page-404/content.php
@@ -0,0 +1,10 @@
+<div class="container-fluid about-content">
+
+  <div class="container">
+
+    <h1>Oops! That page can&rsquo;t be found.</h1>
+    <p>It looks like nothing was found at this location. Click here to go <a href="/">home</a></p>
+
+  </div>
+
+</div>
